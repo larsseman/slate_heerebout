0141acd0fd20a96003b535777ff0d7686b7b0609
Lars Boesenach <lars@lokaalgevonden.nl>
2014-05-15 19:47:25 +0200

    Finished the homepage layout/design

--- dev/php/templates/footer.php
+++ dev/php/templates/footer.php
@@ -1,6 +1,21 @@
-	<!-- Copyright info -->
-	<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?></p>
-	
+	<div class="Footer">
+		<div class="u-gridContainer">
+			<?php if ( is_active_sidebar( 'footer-widgets' ) ) : ?>
+			<div class="Footer-inner">
+				<?php dynamic_sidebar( 'footer-widgets' ); ?>
+			</div>
+			<?php endif; ?>
+		</div>
+	</div>
+
+
+
+	<div class="sub-footer">
+			<!-- Copyright info -->
+			<p>&copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?> - Gemaakt door <a href="http://lokaalgevonden.nl" target="_blank">LokaalGevonden.</a></p>
+		</div>
+		
+		
   <!-- Load jquery from google CDN if possible, with fallback to local version -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/standalone/jquery.min.js"><\/script>')</script>
@@ -15,3 +30,6 @@
 	<?php wp_footer(); ?>
 	</body>
 </html>
+
+
+
--- dev/php/templates/template-Homepage.php
+++ dev/php/templates/template-Homepage.php
@@ -6,7 +6,7 @@ Template Name: Page Homepage
 
 <?php get_header(); ?>
 
-	<div class="u-gridContainer">
+
 		<div class="Content">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -16,54 +16,68 @@ Template Name: Page Homepage
 				<div class="Content-entry">
 
 <!-- ---------------------- Content entry --------------------- -->
-
-<div class="intro-box">
-	<div class="intro-img"></div>
-	<div class="intro-name"><p>Hugo Heerebout</p></div>
-	<div class="intro-text">
-		<p>Ik ben geboren en getogen in Rotterdam en na het Gymnasium Erasmianum in deze stad heb ik Nederlands recht gestudeerd aan de Erasmus Universiteit Rotterdam.</p>
-	</div>
-</div>
-
-
-<div class="vid-padbox">
-	<div class="vid-title"><p>Video impressie</p></div>
-	<div class="vid-box">
-		<div class="vid-video">
-		</div>
-	</div>
-</div>
-
-
-<div class="homeQ-wrapper">
-	<div class="homeQ-box">
-		<div class="homeQ-title"><p>Wat kunnen wij voor u betekenen?</p></div>
-		<div class="homeQ-content">
-			<ul>
-				<li><a href="#">Arbeidsrecht</a></li>
-				<li><a href="#">Personen- en familierecht</a></li>
-				<li><a href="#">Ondernemingsrecht</a></li>
-				<li><a href="#">Geldvorderingen</a></li>
-				<li><a href="#">Bestuursrecht</a></li>
-				<li><a href="#">Onroerend goed</a></li>
-				<li><a href="#">Strafrecht</a></li>
-				<li><a href="#">Erfrecht</a></li>				
-			</ul>
+<div class="u-gridContainer">
+	<div class="intro-box">
+		<div class="intro-img"></div>
+		<div class="intro-name"><p>Hugo Heerebout</p></div>
+		<div class="intro-text">
+			<p>Ik ben geboren en getogen in Rotterdam en na het Gymnasium Erasmianum in deze stad heb ik Nederlands recht gestudeerd aan de Erasmus Universiteit Rotterdam.</p>
 		</div>
 	</div>
 	
-	<div class="homeQ-box">
-		<div class="homeQ-title two"><p>Werkgebied</p></div>
-		<div class="homeQ-content">
-			<p>Heerebout Advocatuur met advocaat Hugo Heerebout, is gevestigd in Nieuw-Vennep, bij Hoofddorp en Schiphol, tussen Haarlem en Amsterdam in en slechts vijf minuten rijden van rijksweg A4.</p>
+	
+	<div class="vid-padbox">
+		<div class="vid-title"><p>Video impressie</p></div>
+		<div class="vid-box">
+			<div class="vid-video">
+			</div>
 		</div>
 	</div>
+</div>
 
 
-	<div class="homeQ-box">
-		<div class="homeQ-title three"><p>Nieuws</p></div>
-		<div class="homeQ-content three">
-			<p>Heerebout Advocatuur met advocaat Hugo Heerebout, is gevestigd in Nieuw-Vennep, bij Hoofddorp en Schiphol, tussen Haarlem en Amsterdam in en slechts vijf minuten rijden van rijksweg A4.</p>
+<div class="homeQ-outerWrapper">
+	<div class="u-gridContainer">
+		<div class="homeQ-wrapper">
+			<div class="homeQ-box">
+				<div class="homeQ-title"><p>Wat kunnen wij voor u betekenen?</p></div>
+				<div class="homeQ-content">
+					<ul>
+						<li><a href="#">Arbeidsrecht</a></li>
+						<li><a href="#">Personen- en familierecht</a></li>
+						<li><a href="#">Ondernemingsrecht</a></li>
+						<li><a href="#">Geldvorderingen</a></li>
+						<li><a href="#">Bestuursrecht</a></li>
+						<li><a href="#">Onroerend goed</a></li>
+						<li><a href="#">Strafrecht</a></li>
+						<li><a href="#">Erfrecht</a></li>				
+					</ul>
+				</div>
+			</div>
+			
+			<div class="homeQ-box">
+				<div class="homeQ-title two"><p>Werkgebied</p></div>
+				<div class="homeQ-content">
+					<p>Heerebout Advocatuur met advocaat Hugo Heerebout, is gevestigd in Nieuw-Vennep, bij Hoofddorp en Schiphol, tussen Haarlem en Amsterdam in en slechts vijf minuten rijden van rijksweg A4.</p>
+				</div>
+				
+				
+				<div class="homeQ-title four"><p>Opzoek naar hulp bij ontslag?</p></div>
+				<div class="homeQ-content four">
+					<a href="#">Klik hier voor uw ontslagwebsite</a>
+				</div>
+			</div>
+		
+		
+			<div class="homeQ-box">
+				<div class="homeQ-title three"><p>Nieuws</p></div>
+				<div class="homeQ-content three">
+					<a href="#">Celstraf voor oplichting en diefstal pinpas.</a>
+					<hr>
+					<a href="#">Bestuursvoorzitter KPMG Nederland Jurgen van Breukelen stapt op als signaal te breken met verleden.</a><hr>
+					<a href="#">De eKantonrechter nu ook toegankelijk voor. </a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -72,11 +86,10 @@ Template Name: Page Homepage
 
 
 					<?php the_content(); ?>
-					<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+					<!-- <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?> -->
 				</div>
 			</article>
 		<?php endwhile; endif; ?>
 		</div>
-	</div>
 
 <?php get_footer(); ?>
